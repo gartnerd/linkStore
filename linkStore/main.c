de8cdfb55fb8b7fc103d7273c28ac58a13b65487
David Gartner <gartnerd@gmail.com>
2015-05-16 14:35:46 -0600

    Initial Commit

---
+++ linkStore/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  linkStore
+//
+//  Created by David M Gartner on 2015-05-16.
+//  Copyright (c) 2015 David. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
