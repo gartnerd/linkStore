42fb6aae4296af3462e71bebab7cc90d0bf4f1b1
David Gartner <gartnerd@gmail.com>
2015-05-16 16:03:56 -0600

    initial commit

--- linkStore/main.c
+++ linkStore/main.c
@@ -7,9 +7,47 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+
+struct icmpCount {
+    int totalRecv;
+    int rtt;
+    int minRtt;
+    int maxRtt;
+    char *remIp;
+    struct icmpCount *link;
+};
+
+typedef struct icmpCount ic;
 
 int main(int argc, const char * argv[]) {
+    int c;
+    char *remoteHost;
+    extern char *optarg;
+    extern int optind, optopt, opterr;
+    
     // insert code here...
     printf("Hello, World!\n");
+    
+    //create a list of locations to ping
+    //from command line arguments
+    while ((c = getopt(argc, argv, ":h:")) != -1) {
+        switch (c) {
+            case 'h':
+                remoteHost = optarg;
+                break;
+            case ':':
+                printf(" -%c requires ip address\n", optopt);
+                break;
+            case '?':
+                printf("unknown arg %c\n", optopt);
+                break;
+        }
+    }
+    
+    printf("remote host addresses %s\n", optarg);
+    
+    //parse the IP addresses and put them into the record structs
+    
     return 0;
 }
